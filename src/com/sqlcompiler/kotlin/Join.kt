99af6072934cec7602b77a47d5a67e65d94cc4ea
Mouaz Jiroudi <37637043+MozJer@users.noreply.github.com>
2019-05-14 23:04:45 +0300

    mozjer03

---
+++ src/com/sqlcompiler/kotlin/Join.kt
@@ -0,0 +1,12 @@
+package com.sqlcompiler.kotlin
+
+class Join(var type: String,
+           var tableName: String,
+           private var tableAlias: String = "",
+           var condition: String,
+           var conditionColumns: ArrayList<String>) {
+
+    override fun toString(): String {
+        return "{ join_type => $type, table_name => $tableName, tableAlias => $tableAlias, condition => $condition, columns => $conditionColumns }"
+    }
+}
