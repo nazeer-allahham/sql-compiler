bcce45e6adccf3716386717fdd5c8332a13581f3
Mohammad Ezalden <mn3199725@gmail.com>
2019-05-16 16:55:45 +0300

    .

--- test.sql
+++ test.sql
@@ -1,1 +1,9 @@
-select * from c where c.id in (select users.id from users)
+select max (c.id) , c.name
+from c join ttt
+on c.id = ttt.id
+join users on c.id = users.id
+where c.id <3 and c.salary<0
+or c.id in (-1,-9)
+GROUP by c.name ,c.id
+order by c.id ,c.name
+d
